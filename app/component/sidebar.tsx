e9f3a609e2a6d6669df76d59d2a32bb8ed6c5629
adevcorps <albert.devguy@gmail.com>
2024-09-27 00:20:05 +0200

    fff

--- app/component/sidebar.tsx
+++ app/component/sidebar.tsx
@@ -1,5 +1,5 @@
 'use client'
-import React, { FunctionComponent } from 'react';
+import React from 'react';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 interface SideBarProps{
