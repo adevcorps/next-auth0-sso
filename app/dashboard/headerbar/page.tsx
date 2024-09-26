ff4a61dfa5099d12a779dac01d74a27a493bddd1
adevcorps <albert.devguy@gmail.com>
2024-09-26 22:36:05 +0200

    sd

--- app/dashboard/headerbar/page.tsx
+++ app/dashboard/headerbar/page.tsx
@@ -8,8 +8,8 @@ import { FiMenu, FiX } from 'react-icons/fi'; // For mobile menu toggle icons
 // background: linear-gradient(270deg, #FF9B04 0%, #FFD601 13.5%, #BBB42D 23%, #6F8E5F 33%, #0057A8 47%, #3806FA 63%, #0D0040 90.15%);
 
 interface HeaderBarProps {
-    toggleVisibility: () => void,
-    isOpen : boolean
+    toggleVisibility: () => void;
+    isOpen : boolean;
 }
 const HeaderBar: FunctionComponent<HeaderBarProps> = ({toggleVisibility, isOpen}) => {
     const hanlder = () => {
