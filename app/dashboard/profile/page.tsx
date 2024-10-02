b47bbcf2573b77a0487134674890fbd9ec430d26
adevcorps <albert.devguy@gmail.com>
2024-10-02 22:48:42 +0200

    profile update

--- app/dashboard/profile/page.tsx
+++ app/dashboard/profile/page.tsx
@@ -153,7 +153,7 @@ const Profile = () => {
                                     className="w-full h-[61px] rounded-lg text-white bg-[#0D0040] flex justify-center items-center">
                                     <svg xmlns="http://www.w3.org/2000/svg" width="26px" height="26px" viewBox="0 0 24 24">
                                         <g stroke="currentColor">
-                                            <circle cx="12" cy="12" r="9.5" fill="none" stroke-linecap="round" stroke-width="1.55">
+                                            <circle cx="12" cy="12" r="9.5" fill="none" strokeLinecap="round" strokeWidth="1.55">
                                                 <animate attributeName="stroke-dasharray" calcMode="spline" dur="1.2s" keySplines="0.42,0,0.58,1;0.42,0,0.58,1;0.42,0,0.58,1" keyTimes="0;0.475;0.95;1" repeatCount="indefinite" values="0 150;42 150;42 150;42 150" />
                                                 <animate attributeName="stroke-dashoffset" calcMode="spline" dur="1.2s" keySplines="0.42,0,0.58,1;0.42,0,0.58,1;0.42,0,0.58,1" keyTimes="0;0.475;0.95;1" repeatCount="indefinite" values="0;-16;-59;-59" />
                                             </circle>
