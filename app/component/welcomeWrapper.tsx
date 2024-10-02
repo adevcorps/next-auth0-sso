d817bc134e2c85436a8395af9cb5121449db194c
adevcorps <albert.devguy@gmail.com>
2024-10-02 18:47:30 +0200

    remove usestate

--- app/component/welcomeWrapper.tsx
+++ app/component/welcomeWrapper.tsx
@@ -1,5 +1,5 @@
 'use client'
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { withPageAuthRequired } from '@auth0/nextjs-auth0/client';
 import { useUser } from '@auth0/nextjs-auth0/client';
 import { useRouter } from "next/navigation";
